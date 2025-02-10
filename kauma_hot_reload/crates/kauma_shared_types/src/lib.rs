aa8de65193101ab66d381a56bb096434a672dfef
kekelp <kekelp@kekelp>
2025-02-10 15:50:24 +0100

    names

--- kauma_hot_reload/crates/kauma_shared_types/src/lib.rs
+++ kauma_hot_reload/crates/kauma_shared_types/src/lib.rs
@@ -1,8 +1,8 @@
 use std::path::PathBuf;
 
-pub const KAUMA_HOT_BUILD_DIR: &str = "kauma_hot_reload";
+pub const KAUMA_HOT_BUILD_DIR: &str = "kauma_hot_reload_target";
 pub const KAUMA_ENV_VAR: &str = "KAUMA_HOT_RELOAD_BUILD";
-pub const KAUMA_SHARED_LIB_NAME: &str = "kauma_shared_lib";
+pub const KAUMA_SHARED_LIB_NAME: &str = "kauma_hot_reload_shared_lib";
 
 pub fn cargo_target_dir() -> PathBuf {
     return std::env::var("CARGO_TARGET_DIR")
